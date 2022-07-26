f595c8c4a7686096ad3e9213138e9040b69eeb15
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-07-26 09:01:02 +0100

    Update results.py

--- Regression_Analysis/output/results.py
+++ Regression_Analysis/output/results.py
@@ -1,5 +1,17 @@
 %run 1_perform_base_case.py
 
+#Base Models 
+BaseModel_203_RSquared = model_203.score(x_203_test, y_203_test)
+BaseModel_204_RSquared = model_204.score(x_204_test, y_204_test)
+BaseModel_205_RSquared = model_205.score(x_205_test, y_205_test)
+BaseModel_228_RSquared = model_228.score(x_228_test, y_228_test)
+BaseModel_233_RSquared = model_233.score(x_233_test, y_233_test)
+BaseModel_251_RSquared = model_251.score(x_251_test, y_251_test)
+BaseModel_253_RSquared = model_253.score(x_253_test, y_253_test)
+BaseModel_260_RSquared = model_260.score(x_260_test, y_260_test)
+BaseModel_267_RSquared = model_267.score(x_267_test, y_267_test)
+BaseModel_339_RSquared = model_339.score(x_339_test, y_339_test)
+
 # naming the y-axis components
 A = [BaseModel_203_RSquared,BaseModel_204_RSquared,BaseModel_205_RSquared,BaseModel_228_RSquared,
      BaseModel_233_RSquared,BaseModel_251_RSquared,BaseModel_253_RSquared,BaseModel_260_RSquared,
