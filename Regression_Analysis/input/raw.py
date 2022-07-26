a9abd959afcdcaa993a24e0a60983df01e79a3ce
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-07-26 08:43:13 +0100

    Update raw.py

--- Regression_Analysis/input/raw.py
+++ Regression_Analysis/input/raw.py
@@ -1,16 +1,15 @@
 # importing all necessary libraries and dataframes
-from google.colab import drive 
 import pandas as pd
 
 #Importing the largest 10 csv files form google drive: 203, 204, 205, 228, 233, 251, 253, 260, 267, 339. 
-drive.mount('/content/drive')
-data_203 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL203.csv')
-data_204 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL204.csv')
-data_205 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL205.csv')
-data_228 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL228.csv')
-data_233 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL233.csv')
-data_251 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL251.csv')
-data_253 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL253.csv')
-data_260 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL260.csv')
-data_267 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL267.csv')
-data_339 = pd.read_csv ('/content/drive/MyDrive/Ross King Research Work/QSAR Raw Data Summer 2022/CHEMBL339.csv')
+#loading datasets 
+CHEMBL203 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL203.csv')
+CHEMBL204 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL204.csv')
+CHEMBL205 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL205.csv')
+CHEMBL228 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL228.csv')
+CHEMBL233 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL233.csv')
+CHEMBL251 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL251.csv')
+CHEMBL253 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL253.csv')
+CHEMBL260 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL260.csv')
+CHEMBL267 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL267.csv')
+CHEMBL339 = pd.read_csv (r'C:\Users\adnan\OneDrive\Documents\TML\Raw_Dataset\CHEMBL339.csv')
