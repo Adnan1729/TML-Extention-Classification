156ef526403968c6cb2214283da1600f5b771581
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-07-26 08:49:48 +0100

    Update and rename 1_perform_base_case to 1_perform_base_case.py

---
+++ Regression_Analysis/codes/1_perform_base_case.py
@@ -0,0 +1,8 @@
+%run 
+#fix_random_state=42
+models = {}
+for data, name in zip(data_sets, names):  
+    #Model building
+    model = RandomForestClassifier(n_estimators=10, random_state=42)
+    model.fit(x_train, y_train)
+    models[name] = model
