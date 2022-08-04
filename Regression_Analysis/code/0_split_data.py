cdfc062ec3550136d6acced29c18fd904857ae94
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-08-04 15:27:17 +0100

    Update 0_split_data.py

--- Regression_Analysis/code/0_split_data.py
+++ Regression_Analysis/code/0_split_data.py
@@ -2,14 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def split_and_save_dataset(dataset, perc = 0.3):
-    df = pd.read_csv(f'../input/raw/{dataset}.csv', index_col=0, header=0)
+    df = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis\\input\\raw\\{dataset}.csv', index_col=0, header=0)
     train, test = train_test_split(df, test_size = perc)
-
-    train.to_csv(f'../input/processed/{dataset}_train.csv')
-    test.to_csv(f'../input/processed/{dataset}_test.csv')
+    train.to_csv(f'C:\\Users\\adnan\\Regression_Analysis\\input\\processed\\{dataset}_train.csv')
+    test.to_csv(f'C:\\Users\\adnan\\Regression_Analysis\\input\\processed\\{dataset}_test.csv')
 
 def main():
-    datasets = ['CHEMBL203', 'CHEMBL204', 'CHEMBL205']
+    datasets = ['CHEMBL203', 'CHEMBL204', 'CHEMBL205', 'CHEMBL228', 'CHEMBL233', 'CHEMBL251', 'CHEMBL253', 'CHEMBL260', 'CHEMBL267', 'CHEMBL339']
     for dataset in datasets:
         split_and_save_dataset(dataset)
 
