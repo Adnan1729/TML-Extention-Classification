f3dda4f0ca2f08cfac0bc1abd3905384276066ff
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-08-04 15:25:25 +0100

    Update 1_perform_base_case.py

--- Regression_Analysis/code/1_perform_base_case.py
+++ Regression_Analysis/code/1_perform_base_case.py
@@ -1,8 +1,16 @@
-%run 0_split_data.py
-#fix_random_state=42
-models = {}
-for data, name in zip(data_sets, names):  
-    #Model building
-    model = RandomForestClassifier(n_estimators=10, random_state=42)
-    model.fit(x_train, y_train)
-    models[name] = model
+import matplotlib.pyplot as plt
+
+ds_name = ["203", "204", "205", "228", "233", 
+     "251", "253", "260", "267", "339"]
+
+plt.plot(ds_name, base_r2_results, color='green', linestyle='dashed', linewidth = 3,
+		marker='o', markerfacecolor='blue', markersize=5)
+
+plt.axhline(y = max(base_r2_results), color = 'r', linestyle = '-', linewidth = 0.3)
+plt.axhline(y = min(base_r2_results), color = 'r', linestyle = '-', linewidth = 0.3)
+
+plt.ylim(0,1)
+plt.xlabel('Dataset_CHEMBLxxx')
+plt.ylabel('R2 Performance')
+plt.title('Base Model Performance')
+plt.show()
