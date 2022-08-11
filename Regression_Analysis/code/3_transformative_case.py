b9d2ae7e348f591a1151d0be81b40d592e3523e7
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-08-11 18:28:33 +0100

    Update 3_transformative_case.py

--- Regression_Analysis/code/3_transformative_case.py
+++ Regression_Analysis/code/3_transformative_case.py
@@ -6,37 +6,31 @@ import pickle
 import matplotlib.pyplot as plt
 
 
-tml_r2_results = []
-
-def read_and_fit_dataset(dataset):
+tml_r2_results=[]
+def tml_model_results(model):
+    base_model = pickle.load(open(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\output\\models\\base\\{model}.sav', 'rb'))
     
-    tml_df_train = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\input\\processed\\{dataset}_train.csv')
-    tml_df_test = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\input\\processed\\{dataset}_test.csv')
+    tml_df_train = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\input\\tml_processed\\tml_dataset_train_{model}.csv')
+    tml_df_test = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\input\\tml_processed\\tml_dataset_test_{model}.csv')
     
     x_train = tml_df_train.drop(columns = ['molecule_id','pXC50'])
     y_train = tml_df_train.pXC50
     x_test = tml_df_test.drop(columns = ['molecule_id','pXC50'])
     y_test = tml_df_test.pXC50
     
-    base_model = pickle.load(open(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\output\\models\\base\\{dataset}.sav', 'rb')
-
-    base_model.fit(x_train,y_train)
-    
     r2_score = base_model.score (x_test,y_test)
     
-    def crt_txt(dataset,r2_score):
-        txt_file = open('C:\\Users\\adnan\\Regression_Analysis\\output\\results\\tml_case.txt', "a")
-        txt_file.write(f'tml {dataset} r2 score is {r2_score}\n')
+    def crt_txt(model,r2_score):
+        txt_file = open('C:\\Users\\adnan\\Regression_Analysis\\output\\results\\base_case.txt', "a")
+        txt_file.write(f'{model} r2 is {r2_score}\n')
         txt_file.close()
-    crt_txt(dataset,r2_score)
+    crt_txt(model,r2_score)
     
     tml_r2_results.append(r2_score)
     
-    
-def main_3():
-    datasets_3 = ['CHEMBL203', 'CHEMBL204', 'CHEMBL205', 'CHEMBL228', 'CHEMBL233', 'CHEMBL251', 'CHEMBL253', 'CHEMBL260', 'CHEMBL267', 'CHEMBL339']
-    for dataset in datasets_3:
-        read_and_fit_dataset(dataset)
+def main_2():
+    modelsets_2 =  ['CHEMBL203', 'CHEMBL204', 'CHEMBL205', 'CHEMBL228', 'CHEMBL233', 'CHEMBL251', 'CHEMBL253', 'CHEMBL260', 'CHEMBL267', 'CHEMBL339']
+    for model in modelsets_2:
+        tml_model_results(model)
 
-main_3()
-                             
+main_2()                   
