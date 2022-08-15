5d23c0cb15d5772779b74c5724845de280816798
Adnan Mahmud <70011012+Adnan1729@users.noreply.github.com>
2022-08-15 03:56:21 +0100

    Update 1_perform_base_case.py

--- Regression_Analysis/code/1_perform_base_case.py
+++ Regression_Analysis/code/1_perform_base_case.py
@@ -1,39 +1,52 @@
 from sklearn.ensemble import RandomForestRegressor 
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 import pandas as pd
 import os
 from sklearn.ensemble import RandomForestRegressor 
 import pickle
 
-base_path = f'C:\\Users\\adnan\\Regression_Analysis\\input\\processed'
+#Defining the RF base model with default settings 
 base_model = RandomForestRegressor()
 
 base_r2_results = []
+base_MSE_results = []
 
 def read_and_fit_dataset(dataset):
     
-    df_train = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis\\input\\processed\\{dataset}_train.csv')
-    df_test = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis\\input\\processed\\{dataset}_test.csv')
+    df_train = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\input\\processed\\{dataset}_train.csv')
+    df_test = pd.read_csv(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\input\\processed\\{dataset}_test.csv')
     
-    #Defining the dependent and independent varibales 
     x_train = df_train.drop(columns = ['molecule_id','pXC50'])
     y_train = df_train.pXC50
     x_test = df_test.drop(columns = ['molecule_id','pXC50'])
     y_test = df_test.pXC50
-   
-    #Fitting the model and storing it as .sav file 
+    
     base_model.fit(x_train,y_train)
-    pickle.dump(base_model, open(f'C:\\Users\\adnan\\Regression_Analysis\\output\\models\\base\\{dataset}.sav', "wb"))
+    pickle.dump(base_model, open(f'C:\\Users\\adnan\\Regression_Analysis_RP050822\\output\\models\\base\\{dataset}.sav', "wb"))
+    y_pred = base_model.predict(x_test)
     
-    r2_score = base_model.score (x_test,y_test)
+    #R2 Score Sumary via generating a txt file    
+    R2_score = r2_score (y_test, y_pred)
     
-    #Creating a single text file to store R2 score. Example: CHEMBL203 r2 score is 0.752974. 
-    def crt_txt(dataset,r2_score):
-        txt_file = open('C:\\Users\\adnan\\Regression_Analysis\\output\\results\\base_case.txt', "a")
-        txt_file.write(f'{dataset} r2 is {r2_score}\n')
+    def crt_txt(dataset, R2_score):
+        txt_file = open('C:\\Users\\adnan\\Regression_Analysis_RP050822\\output\\results\\base_case_r2_scores.txt', "a")
+        txt_file.write(f'{dataset} R2 is {R2_score}\n')
         txt_file.close()
-    crt_txt(dataset,r2_score)
+    crt_txt(dataset,R2_score)
+    
+    #MSE Score Sumary via generating a txt file
+    MSE_score = mean_squared_error(y_test, y_pred)
+    
+    def crt_txt(dataset, MSE_score):
+        txt_file = open('C:\\Users\\adnan\\Regression_Analysis_RP050822\\output\\results\\base_case_MSE-scores.txt', "a")
+        txt_file.write(f'{dataset} mean squared score is {MSE_score}\n')
+        txt_file.close()
+    crt_txt(dataset,MSE_score)
+    #
     
     base_r2_results.append(r2_score)
+    base_MSE_results.append(MSE_score)
     
     
 def main_01():
@@ -43,21 +56,5 @@ def main_01():
 
 main_01()
 
-#Visualisation
-
-import matplotlib.pyplot as plt
-
-ds_name = ["203", "204", "205", "228", "233", 
-     "251", "253", "260", "267", "339"]
-
-plt.plot(ds_name, base_r2_results, color='green', linestyle='dashed', linewidth = 3,
-		marker='o', markerfacecolor='blue', markersize=5)
-
-plt.axhline(y = max(base_r2_results), color = 'r', linestyle = '-', linewidth = 0.3)
-plt.axhline(y = min(base_r2_results), color = 'r', linestyle = '-', linewidth = 0.3)
-
-plt.ylim(0,1)
-plt.xlabel('Dataset_CHEMBLxxx')
-plt.ylabel('R2 Performance')
-plt.title('Base Model Performance')
-plt.show()
+%store base_r2_results
+%store base_MSE_results
